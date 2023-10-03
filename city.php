2e77551adbf666732562668214b531febc19e05d
wlanhage <wlanhage@gmail.com>
2023-10-03 21:50:24 +0200

    added city file with fully working code

--- city.php
+++ city.php
@@ -2,20 +2,20 @@
 
 require('data.php');
 
+$displayedCities = [];
 
+ ?> <div class="citiestext"> The football teams are from the following cities: <br> </div><?php
 
 foreach ($teams as $team) {
     $city = $team['city'];
 
-    ?>
-    <div class="cities">
-    <?php
-    echo $city . '<br>';
-    
-    
-    }?> 
-    </div>
-    <?php
-
-
-?>
+    if (!in_array($city, $displayedCities)) {
+        ?>
+        <div class="cities"> 
+        <?php
+        echo  $city . '<br>';
+        
+        ?> </div> <?php
+        $displayedCities[] = $city;
+    }
+}?>
